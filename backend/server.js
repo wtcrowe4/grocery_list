c963e4801914ed04fee7e04717a7f235213f9380
Thomas Crowe <wcrowe@g.clemson.edu>
2022-09-23 10:51:10 -0400

    Setup simple backend server.

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 8080;
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGO_URI;
+
+
+//Database connection
+mongoose.connect(mongoURI, () => console.log(`MongoDB connected at ${mongoURI}`));
+
+//Middleware
+app.use(express.json());
+
+//Routes
+
+
+
+
+//Server
+app.listen(port, () => console.log(`Server running on port ${port}`));
