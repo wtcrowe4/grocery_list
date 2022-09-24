ff31e4da733a823a7536613f5963e144ada39a2d
Courtney Carroll <bcakescw@gmail.com>
2022-09-24 10:50:40 -0400

    header completed for grocery list page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Grocery List</h1>
-      </header>
+      <Header></Header>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,10 @@ import React  from 'react'
 
 export default function Header() {
     return (
-        <div>
-            
+        <div className="card bg-info text-center text-light rounded-0">
+            <h1 className="display-4">
+                <i className="fas.fa-clipboard=list.mr-3"></i> <span className="text-dark mr-3">Grocery List</span>
+            </h1>
         </div>
     )
 }
