6878a6f8c2b4a76392455707449db7d515109530
Courtney Carroll <bcakescw@gmail.com>
2022-09-24 10:55:26 -0400

    grocery form created

---
+++ src/components/addGroceries.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class addGroceries extends Component {
+    render () {
+        return (
+            <form>
+                <input type="text" className="form-control rounded-0"/>
+                <button></button>
+            </form>
+        )
+    }
+}
