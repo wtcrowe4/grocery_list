5c621ff12c69efbd2918977dde3f1b1e8e622def
Courtney Carroll <bcakescw@gmail.com>
2022-09-26 18:13:34 -0400

    context provider created

--- src/context.js
+++ src/context.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
-export default class context extends Component {
+//created a context and then created a class called Provider 
+// which returns context.provder which has value
+//wanted to wrap this around the whole application
+
+const Context = React.createContext()
+export class Provider extends Component {
+    state={
+        groceries:[
+            {id:1,
+            title:"Grocery",
+            complete: false}
+        ]
+    }
     render () {
         return (
-            <div>
-                
-            </div>
+            <Context.Provider value={this.state}>
+                {this.props.children}
+            </Context.Provider>
         )
     }
 }
+
+export const Consumer = Context.Consumer
