aba42ff3a3fe145f3534a7d41440a3783c342588
Courtney Carroll <bcakescw@gmail.com>
2022-09-26 18:15:52 -0400

    app wrapped in provider

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Header from './components/Header';
 import AddGrocery from './components/AddGrocery';
 import Groceries from './components/Groceries';
+import { Provider } from './context';
 
 function App() {
   return (
-    <div className="app-container">
+    <Provider>
+       <div className="app-container">
       <Header></Header>
       <AddGrocery></AddGrocery>
       <Groceries></Groceries>
     </div>
+    </Provider>
   );
 }
 
