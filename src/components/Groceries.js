e319e954f410d09fd82bdcaffd70cb10b7984774
Courtney Carroll <bcakescw@gmail.com>
2022-09-26 18:23:32 -0400

    imported consumer and title

--- src/components/Groceries.js
+++ src/components/Groceries.js
@@ -1,15 +1,19 @@
 import Grocery from "./Grocery";
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Consumer } from '..context/'
 
 export default class Groceries extends Component {
-    render () {
+    render() {
         return (
-           <div>
-            <Grocery></Grocery>
-            <Grocery></Grocery>
-            <Grocery></Grocery>
-            <Grocery></Grocery>
-            <Grocery></Grocery>
-           </div>
-    )}
- }
+            <Consumer>
+                {value => {
+                    const { Groceries } = value;
+                    return Groceries.map(t => <Grocery Grocery={t}> </Grocery>)
+                }}
+            </Consumer>
+        )
+    }
+}
+
+//imported Consumer which pulls the value of the whole state and making map of the grocery
+//for each grocery, it returns one grocery
--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 
 export default class Grocery extends Component {
     render () {
+        const {title} = this.props.grocery
         return (
            <h3 className="text-dark text-center p-1 bg-light border-bottom">
-            <i className="far fa-times-circle fa-sm float-left m-1 text-danger"></i>Grocery
+            <i className="far fa-times-circle fa-sm float-left m-1 text-danger"></i>{Grocery}
             <input type="checkbox" className="m-2 float-right"/>
            </h3>
     )}
