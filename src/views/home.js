1b1b0b054b9c48e36e008267f82163cd2e940c7c
BDedafoe <bmd6260@gmail.com>
2022-09-26 20:37:37 -0500

    added a calendar to the home page

--- src/views/home.js
+++ src/views/home.js
@@ -1,24 +1,15 @@
 import React, { useState } from 'react';
+import '../App.css';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+
 
 function Home () {
-  const [value, setValue] = useState(new Date());
-
-  function onChange(nextValue) {
-    setValue(nextValue);
-  }
+  const [value, onChange] = useState(new Date());
 
   return (
-    
-    <div className="Calendar">
-      <div>
-        <Calendar onChange={onChange} value={value} />
-      </div>
+    <div>
+      <Calendar onChange={onChange} value={value} />
     </div>
-
   );
 }
-
 export default Home;
-
