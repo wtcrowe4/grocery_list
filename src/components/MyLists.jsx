3d7064ca8b73dfdf3e169a5088855f8ae7216cf2
Thomas Crowe <wcrowe@g.clemson.edu>
2022-10-01 09:23:45 -0400

    My lists showing all lists instead of user

--- src/components/MyLists.jsx
+++ src/components/MyLists.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector, } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 //import listSlice from '../features/lists/listSlice';
-import { create } from '../features/lists/listSlice';
+import { createList, getMyLists } from '../features/lists/listSlice';
 
 
 const MyLists = () => {
@@ -11,7 +11,7 @@ const MyLists = () => {
     // const { user } = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user')) : null;
     const user = useSelector(state => state.auth ? state.auth : null);
     const lists = useSelector(state => state.lists);
-    
+    //const [myLists, setMyLists] = useState([])
     
     const [listName, setListName] = useState('');
     
@@ -21,7 +21,42 @@ const MyLists = () => {
         if(!user) {
             navigate('/login' , { replace: true });
         }
-    }, [user, navigate]);
+    
+        
+    
+        dispatch(getMyLists(user.user._id));
+        // const getLists = async () => {
+        //     const allLists = await dispatch(getMyLists());
+        //     console.log(user.user.user._id);
+        //     console.log(allLists.payload);
+        //     allLists.payload.map(list => {
+        //         if (list.userId === user.user.user._id) {
+        //             console.log(list.title);
+        //             //setMyLists([...myLists, list]);
+        //             return list;
+        //         } else {
+        //             return null;
+        //         }
+        // })
+    // }}  
+    
+    //setMyLists([...myLists, getLists()]);
+
+         
+        
+ 
+    
+    
+    
+    
+    
+    }, [user, navigate, dispatch]);
+
+    
+    
+    
+    
+    
 
     //Fix error on welcome message when user is null
     if (!user) {
@@ -39,33 +74,57 @@ const MyLists = () => {
         console.log(listName)
         console.log(newList);
         //listSlice.addList(newList);
-        dispatch(create(newList));   
+        dispatch(createList(newList));   
     }
 
 
-    //code to render all lists for user in database
+
+   
+    
+
+
+
+
     const renderLists = () => {
-        const myLists = lists.lists;
-        console.log(myLists);
-        return (
-            <div>
+        
+        if (lists.lists.length === 0) {
+            return <p>You have no lists</p>
+        } else {
+            return (
                 <ul>
-                    {myLists.map((list) => {
-                        if(!list._id) {
-                            return null;
-                        } else {
-                            return (
-                                <a href={'/dashboard'}>
-                                    <li key={list._id}>{list.title}</li> 
-                                </a>
-                            )
-                        }
-                    })}
-                    
+                    {lists.lists.map(list => (
+                        <a href={'/dashboard'} key={list._id}>
+                            <li key={list._id}>{list.title}</li>
+                        </a>
+                    ))}
                 </ul>
-            </div>
-        )
+            )
+        }
     }
+       
+        
+        
+      
+        
+    
+        
+        
+    //     return (
+    //        <div>
+    //            <ul>
+                   
+                          
+    //                            <a key={list._id} href={'/dashboard'}>
+    //                                <li key={myLists._id}>{myLists.title}</li> 
+    //                            </a>
+                         
+                
+                   
+    //            </ul>
+    //        </div>
+    //    )
+
+    
 
 
     return(
@@ -78,9 +137,11 @@ const MyLists = () => {
                 </div>
                 <button type="submit" className="btn btn-primary">Create List</button>
             </form>
+            {/* <p>{[myLists]}</p> */}
             {renderLists()}
 
-        </div>
+            </div>
+
     )
 }
 
--- src/features/lists/listService.js
+++ src/features/lists/listService.js
@@ -14,6 +14,31 @@ const create = async (list, token) => {
 };
 
 //Get all lists
+const all = async (token, userId) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        // params: {
+        //     userId: userId
+        // }
+    }
+    const response = await axios.get(`${api_url}/all`, config);
+    // response.data.forEach(list => {
+    //     if (list.userId === userId) {
+    //         console.log(list);
+    //         return list;
+    //     } else {
+    //         return null;
+    //     }
+    
+    
+   return response.data 
+    // });
+};
+    
+//     return myLists;
+// };
 
 
 
@@ -22,7 +47,7 @@ const create = async (list, token) => {
 
 const listService = {
     create,
-    // all,
+    all,
     // get,
     // update,
     // deleteList
